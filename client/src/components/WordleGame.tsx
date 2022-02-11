6071c622ff56df2eca333779341656b40eca62fd
Joe <37256060+JosephDavidTalbot@users.noreply.github.com>
2022-02-11 08:41:01 -0600

    Refs #14: Refactored game logic to use hooks.

--- client/src/components/WordleGame.tsx
+++ client/src/components/WordleGame.tsx
@@ -1,18 +1,63 @@
 import { useState } from "react";
 import WordleInterface from "./WordleInterface"
 
+type HistoryEntry = {
+    guess: string;
+    accuracy: string[];
+}
+
 type GameState = {
-    history: string[];
+    history: HistoryEntry[];
     guessNumber: number;
+    guessesAllowed: number;
     secretWord: string;
     hasWon: boolean;
 }
 
-const [gameState, updateGameState] = useState<GameState>({
-    hasWon: false
-} as GameState);
+const CheckGuess = (guess: string) => {
+    const [gameState, updateGameState] = useState<GameState>({
+        guessesAllowed: 6,
+        hasWon: false
+    } as GameState);
 
-const checkGuess = (guess: string) => {
+    if(gameState.history.length >= gameState.guessesAllowed || gameState.hasWon) {
+        return;
+    }
+    const history = gameState.history.slice(0, gameState.guessNumber-1);
+    const target = gameState.secretWord;
+    var accuracy = [];
+    //Shorthand will be used for accuracy. Whitespace indicates absence of the corresponding character. X indicates presence but in a different location. O indicates presence in that location.
+    for(var i=0; i<guess.length; i++) {
+        switch(target.indexOf(guess[i])) { //For each character in guess, check if it's in the secret word.
+            case -1: //If not, return whitespace.
+                accuracy.push(' ');
+                break;
+            case i: //If it's in the same place it was in the guess, return O.
+                accuracy.push('O');
+                break;
+            default://Otherwise, it has to be present but misplaced. Return X.
+                accuracy.push('X');
+                break;
+        }
+    }
+    var victory = false;
+    if(accuracy === ['O','O','O','O','O']){ //Checks if the player has won, and if so, sets the victory flag to true.
+        victory = true;
+    }
+    updateGameState({
+        history: history.concat({
+            guess: guess,
+            accuracy: accuracy,
+        }),
+        guessNumber: history.length,
+        //guessesAllowed and secretWord are both constants. What's the ideal way to deal with these here?
+        //guessesAllowed: this.props.guessesAllowed,
+        //secretWord: this.props.secretWord,
+        guessesAllowed: gameState.guessesAllowed,
+        secretWord: gameState.secretWord,
+        hasWon: victory,
+    });
+
     return false;
 }
 
@@ -20,7 +65,7 @@ const checkGuess = (guess: string) => {
 export class WordleGame extends React.Component {
     constructor(props: WordleInterface) {
         super(props);
-        this.state = {
+        gameState = {
             history: [],
             guessNumber: 1,
             guessesAllowed: props.guessesAllowed,
@@ -30,11 +75,11 @@ export class WordleGame extends React.Component {
     }
 
     checkGuess(guess: string) {
-        if(this.state.history.length >= this.state.guessesAllowed || this.state.hasWon) {
+        if(gameState.history.length >= gameState.guessesAllowed || gameState.hasWon) {
             return;
         }
-        const history = this.state.history.slice(0, this.state.guessNumber-1);
-        const target = this.state.secretWord;
+        const history = gameState.history.slice(0, gameState.guessNumber-1);
+        const target = gameState.secretWord;
         var accuracy = [];
         //Shorthand will be used for accuracy. Whitespace indicates absence of the corresponding character. X indicates presence but in a different location. O indicates presence in that location.
         for(var i=0; i<guess.length; i++) {
@@ -68,7 +113,13 @@ export class WordleGame extends React.Component {
 }
 */
 export const WordleGame = () => {
-    return <div>
-        Wordle!
-    </div>
+    return (
+        <div>
+            Wordle!
+
+
+
+            <div id="msg"></div>
+        </div>
+    );
 }
